d3da40677a7c9401fe7ba2713195bacdaf098999
eldarst <st073513@student.spbu.ru>
2022-06-23 15:08:08 +0300

    V1.2 - Change properties

--- src/main/java/com/yandex/yandexmarket/YandexMarketApplication.java
+++ src/main/java/com/yandex/yandexmarket/YandexMarketApplication.java
@@ -2,6 +2,9 @@ package com.yandex.yandexmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
 
 @SpringBootApplication
 public class YandexMarketApplication {
